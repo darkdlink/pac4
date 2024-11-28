413a7d69098f1bdaa2ed794b74446044ea0215e6
Sidney C de Oliveira Jr <sidneycardosojo@gmail.com>
2024-11-28 17:34:51 -0300

    Att de telas

--- beautycare_app/lib/telas/tela_cadastro_cliente.dart
+++ beautycare_app/lib/telas/tela_cadastro_cliente.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class TelaCadastroCliente extends StatefulWidget {
   const TelaCadastroCliente({super.key});
@@ -8,102 +10,100 @@ class TelaCadastroCliente extends StatefulWidget {
 }
 
 class _TelaCadastroClienteState extends State<TelaCadastroCliente> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cadastro de Cliente'),
-        backgroundColor: const Color(0xFFDB9B83),
-      ),
-      body: SingleChildScrollView(
-        //rolagem da tela
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment:
-                CrossAxisAlignment.stretch, // Estica os elementos horiz
-            children: [
-              const Center(
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundImage:
-                      AssetImage('assets/imagens/foto_cliente.png'),
-                ),
-              ),
-              const SizedBox(height: 20),
-              // Campos da ficha de anamnese
-              _construirCampoTexto('Nome'),
-              _construirCampoData('Data de Nascimento'),
-              _construirCampoTexto('Escolaridade'),
-              _construirCampoTexto('Profissão'),
-              _construirCampoTexto('Estado Civil'),
-              const SizedBox(height: 20),
-              const Text(
-                'Consulta Avaliativa Integrativa',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const Divider(),
-              _construirPerguntaSimNao(
-                  'Já realizou algum tratamento estético?'),
-              _construirPerguntaSimNao('Tem filhos?'),
-              _construirPerguntaSimNao('Está amamentando?'),
-              _construirPerguntaOpcoes(
-                  'Como funciona seu intestino', ['Ótimo', 'Bom', 'Regular']),
-              _construirPerguntaOpcoes(
-                  'Ingere água?', ['Muito', 'Médio', 'Pouco']),
-              _construirPerguntaOpcoes(
-                  'Hábitos alimentares?', ['Saudável', 'Regular', 'Péssimo']),
-              _construirPerguntaSimNao('Possui intolerância alimentar?'),
-              _construirPerguntaSimNao('Ingere bebida alcoólica?'),
-              _construirPerguntaSimNao('Fuma?'),
-              _construirPerguntaOpcoes(
-                  'Como é o seu sono?', ['Bom', 'Regular', 'Ruim']),
-              _construirPerguntaSimNao('Pratica atividade física?'),
-              _construirPerguntaSimNao('Usa cosméticos?'),
-              _construirPerguntaSimNao('Tem alergias ou irritações?'),
-              _construirPerguntaSimNao('Tem queloide?'),
-              _construirPerguntaSimNao('Pele mancha com facilidade?'),
-              _construirPerguntaSimNao('Tem patologias?'),
-              _construirPerguntaSimNao('Tem distúrbio hormonal?'),
-              _construirPerguntaSimNao('Usa anticoncepcional?'),
-              _construirPerguntaSimNao('Uso de Antidepressivo?'),
-              _construirCampoTexto('Faz uso de medicamento?'),
-              _construirPerguntaSimNao('Possui deficiência de vitaminas?'),
-              _construirPerguntaSimNao('Unhas e cabelos fracos?'),
-              const SizedBox(height: 20),
-              const Text(
-                'Avaliação da pele',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const Divider(),
-              _construirCampoTexto('Observações sobre a pele'),
-              const SizedBox(height: 20),
-              const Text(
-                'Tratamento',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const Divider(),
-              _construirCampoTexto('Descrição do tratamento'),
-              const SizedBox(height: 20),
-              const Text(
-                'Produtos para usar em casa',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const Divider(),
-              _construirCampoTexto('Recomendações de produtos'),
-              const SizedBox(height: 40),
-            ],
-          ),
-        ),
-      ),
-    );
+  // Variáveis para armazenar as respostas das perguntas
+  bool? _tratamentoEstetico;
+  bool? _temFilhos;
+  bool? _amamentando;
+  String? _intestino;
+  String? _agua;
+  String? _habitosAlimentares;
+  bool? _intoleranciaAlimentar;
+  bool? _bebidaAlcoolica;
+  bool? _fuma;
+  String? _sono;
+  bool? _atividadeFisica;
+  bool? _usaCosmeticos;
+  bool? _alergiasIrritacoes;
+  bool? _temQueloide;
+  bool? _peleManchaFacilidade;
+  bool? _temPatologias;
+  bool? _temDisturbioHormonal;
+  bool? _usaAnticoncepcional;
+  bool? _usoAntidepressivo;
+  bool? _deficienciaVitaminas;
+  bool? _unhasCabelosFracos;
+
+  // Controladores de texto
+  final TextEditingController _nomeController = TextEditingController();
+  final TextEditingController _escolaridadeController = TextEditingController();
+  final TextEditingController _profissaoController = TextEditingController();
+  final TextEditingController _estadoCivilController = TextEditingController();
+  final TextEditingController _medicamentoController = TextEditingController();
+  final TextEditingController _observacoesPeleController =
+      TextEditingController();
+  final TextEditingController _descricaoTratamentoController =
+      TextEditingController();
+  final TextEditingController _recomendacoesProdutosController =
+      TextEditingController();
+  final TextEditingController _dataNascimentoController =
+      TextEditingController();
+
+  // Variável para armazenar a imagem da cliente
+  File? _image;
+
+  // Controlador de imagem
+  final ImagePicker _picker = ImagePicker();
+
+  // Função para validar os campos obrigatórios
+  bool _validarCampos() {
+    if (_nomeController.text.isEmpty ||
+        _dataNascimentoController.text.isEmpty) {
+      return false;
+    }
+    return true;
   }
 
-  // Função: construir um campo de texto
-  Widget _construirCampoTexto(String label) {
+  // Função para pegar a imagem da galeria ou da câmera
+  Future<void> _pickImage() async {
+    // Exibe o diálogo para o usuário escolher entre tirar foto ou pegar da galeria
+    final ImageSource? source = await showDialog<ImageSource>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Escolha uma opção'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context, ImageSource.camera);
+            },
+            child: const Text('Tirar Foto'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context, ImageSource.gallery);
+            },
+            child: const Text('Escolher da Galeria'),
+          ),
+        ],
+      ),
+    );
+
+    if (source != null) {
+      // Pega a imagem com base na escolha do usuário
+      final XFile? pickedFile = await _picker.pickImage(source: source);
+      if (pickedFile != null) {
+        setState(() {
+          _image = File(pickedFile.path); // Armazena a imagem selecionada
+        });
+      }
+    }
+  }
+
+  // Função para construir um campo de texto
+  Widget _construirCampoTexto(String label, TextEditingController controller) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: TextField(
+        controller: controller,
         decoration: InputDecoration(
           labelText: label,
           border: const OutlineInputBorder(),
@@ -112,37 +112,64 @@ class _TelaCadastroClienteState extends State<TelaCadastroCliente> {
     );
   }
 
-  // Função:construir um campo de data
-  Widget _construirCampoData(String label) {
+  // Função para construir um campo de data
+  Widget _construirCampoData(String label, TextEditingController controller) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: TextField(
+        controller: controller,
         decoration: InputDecoration(
           labelText: label,
           border: const OutlineInputBorder(),
           suffixIcon: const Icon(Icons.calendar_today),
         ),
-        onTap: () {
-          // Ação para selecionar a data tem que descobrir como implementar
+        onTap: () async {
+          DateTime? selectedDate = await showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(1900),
+            lastDate: DateTime.now(),
+          );
+          if (selectedDate != null) {
+            setState(() {
+              controller.text = '${selectedDate.toLocal()}'.split(' ')[0];
+            });
+          }
         },
       ),
     );
   }
 
-  // Função:construir uma pergunta com opções Sim/Não
-  Widget _construirPerguntaSimNao(String pergunta) {
+  // Função para construir perguntas com respostas Sim/Não
+  Widget _construirPerguntaSimNao(String pergunta, bool? resposta) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: Row(
         children: [
           Text(pergunta),
-          const Spacer(), //  opções para a direita
-          const Row(
+          const Spacer(),
+          Row(
             children: [
-              Radio(value: true, groupValue: null, onChanged: null),
-              Text('Sim'),
-              Radio(value: false, groupValue: null, onChanged: null),
-              Text('Não'),
+              Radio<bool>(
+                value: true,
+                groupValue: resposta,
+                onChanged: (value) {
+                  setState(() {
+                    resposta = value;
+                  });
+                },
+              ),
+              const Text('Sim'),
+              Radio<bool>(
+                value: false,
+                groupValue: resposta,
+                onChanged: (value) {
+                  setState(() {
+                    resposta = value;
+                  });
+                },
+              ),
+              const Text('Não'),
             ],
           ),
         ],
@@ -150,8 +177,9 @@ class _TelaCadastroClienteState extends State<TelaCadastroCliente> {
     );
   }
 
-  // Função:construir uma pergunta com múltiplas opções
-  Widget _construirPerguntaOpcoes(String pergunta, List<String> opcoes) {
+  // Função para construir perguntas com múltiplas opções (RadioListTile)
+  Widget _construirPerguntaOpcoes(
+      String pergunta, List<String> opcoes, String? resposta) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: Column(
@@ -162,16 +190,188 @@ class _TelaCadastroClienteState extends State<TelaCadastroCliente> {
           Wrap(
             children: opcoes
                 .map((opcao) => Expanded(
-                      child: RadioListTile(
+                      child: RadioListTile<String>(
                         title: Text(opcao),
                         value: opcao,
-                        groupValue: null,
-                        onChanged: null,
+                        groupValue: resposta,
+                        onChanged: (value) {
+                          setState(() {
+                            resposta = value;
+                          });
+                        },
                       ),
                     ))
                 .toList(),
           ),
         ],
+      ),
+    );
+  }
+
+  // Função para o botão de salvar com validação
+  Widget _construirBotaoSalvar() {
+    return ElevatedButton(
+      onPressed: () {
+        if (_validarCampos()) {
+          // Aqui você pode navegar para outra tela ou salvar os dados
+          ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Cadastro concluído com sucesso!')));
+        } else {
+          // Exibe uma mensagem de erro
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+              content:
+                  Text('Por favor, preencha todos os campos obrigatórios!')));
+        }
+      },
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20, // Menos padding para um botão menor
+          vertical: 10, // Menor altura
+        ),
+        backgroundColor: const Color(0xFF966C5C), // Cor do botão
+      ),
+      child: const Text(
+        'Salvar Cadastro',
+        style: TextStyle(fontSize: 16), // Menor fonte
+      ),
+    );
+  }
+
+  // Controlador de rolagem
+  final ScrollController _scrollController = ScrollController();
+
+  // Função para rolar para o topo
+  void _rolarParaTopo() {
+    _scrollController.animateTo(
+      0.0, // Rolando para o topo
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Volta para a tela anterior
+          },
+        ),
+        title: const Text('Cadastro de Cliente'),
+        backgroundColor: const Color(0xFFDB9B83),
+      ),
+      body: SingleChildScrollView(
+        controller: _scrollController,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Círculo de avatar, onde pode adicionar ou alterar a foto
+              GestureDetector(
+                onTap: _pickImage, // Ao clicar, abre a escolha de imagem
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: _image != null
+                      ? FileImage(
+                          _image!) // Se uma imagem for escolhida, exibe a imagem
+                      : const AssetImage('assets/imagens/foto_cliente.png')
+                          as ImageProvider, // Imagem padrão caso não tenha sido escolhida
+                ),
+              ),
+              const SizedBox(height: 20),
+              _construirCampoTexto('Nome', _nomeController),
+              _construirCampoData(
+                  'Data de Nascimento', _dataNascimentoController),
+              _construirCampoTexto('Escolaridade', _escolaridadeController),
+              _construirCampoTexto('Profissão', _profissaoController),
+              _construirCampoTexto('Estado Civil', _estadoCivilController),
+              const SizedBox(height: 20),
+              const Text(
+                'Consulta Avaliativa Integrativa',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const Divider(),
+              _construirPerguntaSimNao('Já realizou algum tratamento estético?',
+                  _tratamentoEstetico),
+              _construirPerguntaSimNao('Tem filhos?', _temFilhos),
+              _construirPerguntaSimNao('Está amamentando?', _amamentando),
+              _construirPerguntaOpcoes('Como funciona seu intestino',
+                  ['Ótimo', 'Bom', 'Regular'], _intestino),
+              _construirPerguntaOpcoes(
+                  'Ingere água?', ['Muito', 'Médio', 'Pouco'], _agua),
+              _construirPerguntaOpcoes('Hábitos alimentares?',
+                  ['Saudável', 'Regular', 'Péssimo'], _habitosAlimentares),
+              _construirPerguntaSimNao(
+                  'Possui intolerância alimentar?', _intoleranciaAlimentar),
+              _construirPerguntaSimNao(
+                  'Ingere bebida alcoólica?', _bebidaAlcoolica),
+              _construirPerguntaSimNao('Fuma?', _fuma),
+              _construirPerguntaOpcoes(
+                  'Como é o seu sono?', ['Bom', 'Regular', 'Ruim'], _sono),
+              _construirPerguntaSimNao(
+                  'Pratica atividade física?', _atividadeFisica),
+              _construirPerguntaSimNao('Usa cosméticos?', _usaCosmeticos),
+              _construirPerguntaSimNao(
+                  'Tem alergias ou irritações?', _alergiasIrritacoes),
+              _construirPerguntaSimNao('Tem queloide?', _temQueloide),
+              _construirPerguntaSimNao(
+                  'Pele mancha com facilidade?', _peleManchaFacilidade),
+              _construirPerguntaSimNao('Tem patologias?', _temPatologias),
+              _construirPerguntaSimNao(
+                  'Tem distúrbio hormonal?', _temDisturbioHormonal),
+              _construirPerguntaSimNao(
+                  'Usa anticoncepcional?', _usaAnticoncepcional),
+              _construirPerguntaSimNao(
+                  'Uso de Antidepressivo?', _usoAntidepressivo),
+              _construirCampoTexto(
+                  'Faz uso de medicamento?', _medicamentoController),
+              _construirPerguntaSimNao(
+                  'Possui deficiência de vitaminas?', _deficienciaVitaminas),
+              _construirPerguntaSimNao(
+                  'Unhas e cabelos fracos?', _unhasCabelosFracos),
+              const SizedBox(height: 20),
+              const Text(
+                'Avaliação da pele',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const Divider(),
+              _construirCampoTexto(
+                  'Observações sobre a pele', _observacoesPeleController),
+              const SizedBox(height: 20),
+              const Text(
+                'Tratamento',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const Divider(),
+              _construirCampoTexto(
+                  'Descrição do tratamento', _descricaoTratamentoController),
+              const SizedBox(height: 20),
+              const Text(
+                'Produtos para usar em casa',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const Divider(),
+              _construirCampoTexto('Recomendações de produtos',
+                  _recomendacoesProdutosController),
+              const SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Seta que volta para o topo
+                  IconButton(
+                    icon: const Icon(Icons.arrow_upward),
+                    onPressed: _rolarParaTopo,
+                  ),
+                  // Botão de salvar
+                  _construirBotaoSalvar(),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- beautycare_app/lib/telas/tela_login.dart
+++ beautycare_app/lib/telas/tela_login.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:beautycare_app/telas/tela_menu.dart'; // Importe a tela de menu
 
-class TelaLogin extends StatelessWidget {
+class TelaLogin extends StatefulWidget {
   const TelaLogin({super.key});
+
+  @override
+  _TelaLoginState createState() => _TelaLoginState();
+}
+
+class _TelaLoginState extends State<TelaLogin> {
+  final _formKey = GlobalKey<FormState>(); // Adicionando chave para o formulário
+  final TextEditingController _usuarioController = TextEditingController();
+  final TextEditingController _senhaController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -10,65 +19,90 @@ class TelaLogin extends StatelessWidget {
       backgroundColor: const Color(0xFFDB9B83), // Cor do fundo
       body: Center(
         child: SingleChildScrollView(
-          //rolagem se o conteudo for maior que a tela
+          // Rolagem se o conteúdo for maior que a tela
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.only(bottom: 32.0),
-                child: Image.asset('assets/imagens/logo.png'), // Substituir lgo
+                child: Image.asset('assets/imagens/logo.png'), // Substituir logo
               ),
+              // Formulário de login
               Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Usuário',
-                    prefixIcon: const Icon(Icons.person),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    fillColor: Colors.white,
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      // Campo de Usuário
+                      TextFormField(
+                        controller: _usuarioController,
+                        decoration: InputDecoration(
+                          hintText: 'Usuário',
+                          prefixIcon: const Icon(Icons.person),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor, insira o nome de usuário';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16.0), // Espaço entre os campos
+                      // Campo de Senha
+                      TextFormField(
+                        controller: _senhaController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Senha',
+                          prefixIcon: const Icon(Icons.lock),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor, insira a senha';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 24.0), // Espaço entre o campo e o botão
+                      // Botão de Login
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF966C5C), // Cor do botão
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 15),
+                          textStyle: const TextStyle(fontSize: 18),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                          ),
+                        ),
+                        onPressed: () {
+                          // Verificar se o formulário é válido
+                          if (_formKey.currentState!.validate()) {
+                            // Se for válido, navega para a tela de menu
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const TelaMenu()),
+                            );
+                          }
+                        },
+                        child: const Text('Login'),
+                      ),
+                    ],
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: 'Senha',
-                    prefixIcon: const Icon(Icons.lock),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 24.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFF966C5C), // Cor do botão
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 50, vertical: 15),
-                    textStyle: const TextStyle(fontSize: 18),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                    ),
-                  ),
-                  onPressed: () {
-                    //para a tela de menu após o login
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const TelaMenu()),
-                    );
-                  },
-                  child: const Text('Login'),
                 ),
               ),
             ],
